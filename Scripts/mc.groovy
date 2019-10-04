e3842cd6cbfed33c694d19397aa8a910ad5f8680
Dina Gamal Mahmoud labib <46623409+DinaGamalMahmoud@users.noreply.github.com>
2019-10-04 11:59:49 +0200

    Create mc.groovy

---
+++ Scripts/mc.groovy
@@ -0,0 +1,24 @@
+#!/usr/bin/env groovy
+import groovy.json.JsonSlurper
+
+
+println "hello world"
+   def body = '{"name":"dina.gamal1@vodafone.com","password":"Voda@123"}'
+                def http = new URL("https://hpmc12.mobilecenter.io/rest/client/login").openConnection() as HttpURLConnection
+    http.setRequestMethod('POST')
+    http.setDoOutput(true)
+    http.setRequestProperty("Content-Type", "application/json; charset=UTF-8")
+
+    http.outputStream.write(body.getBytes("UTF-8"))
+  
+    http.connect()
+
+    def responsLogin = [:]    
+
+    if (http.responseCode == 200) {
+            println "200" 
+        responsLogin = new JsonSlurper().parseText(http.inputStream.getText('UTF-8'))
+    } else {
+    
+       responsLogin = new JsonSlurper().parseText(http.errorStream.getText('UTF-8'))
+    }
